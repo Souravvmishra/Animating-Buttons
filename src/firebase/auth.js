bd5dfb63a7eb246b06fde6974f0576de99a4f923
Spyware007 <omgawandeofficial9834899149@gmail.com>
2023-06-04 18:10:41 +0530

    added env variables

--- src/firebase/auth.js
+++ src/firebase/auth.js
@@ -1,20 +1,19 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import {getFirestore} from "firebase/firestore";
-
+import { getFirestore } from "firebase/firestore";
+console.log(process.env);
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDxxpMJb2oBSUFrbqWo3gBF1sefOk1A96s",
-  authDomain: "animating-buttons.firebaseapp.com",
-  databaseURL: "https://animating-buttons-default-rtdb.firebaseio.com",
-  projectId: "animating-buttons",
-  storageBucket: "animating-buttons.appspot.com",
-  messagingSenderId: "813372452114",
-  appId: "1:813372452114:web:fb41ec5b117400d77657cb",
-  measurementId: "G-B2J5KD293M"
-
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
+  measurementId: process.env.REACT_APP_measurementId,
 };
+console.log(firebaseConfig);
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
